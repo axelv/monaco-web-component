8367540b6b6b8598dfc75eeada4ceca6c91532da
Axel Vanraes <axel.vanraes@tiro.health>
2024-03-21 13:47:45 +0100

    fix typo monaco attrs

--- monaco.js
+++ monaco.js
@@ -7,7 +7,7 @@ class MonacoEditor extends HTMLElement {
     this.contentType = this.getAttribute("content-type");
     this.name = this.getAttribute("name");
     this.filename = this.getAttribute("filename");
-    this.value = this.getAttribute("defaultValue");
+    this.value = this.getAttribute("defaultvalue");
   }
   static get observedAttributes() {
     return ["content-type", "name", "filename"];
